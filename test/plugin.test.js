b1798cee31635cb26d4a7061b930e8a87ea83351
Dean McDonnell <mcdonnelldean@outlook.com>
2015-05-19 21:57:13 +0100

    See Description - fixed issue with dotfiles not be true for minimatch - added tests to ensure support for files with ~ in them

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -6,6 +6,7 @@ var filesMock = {
   'index.js': {},
   'posts/one.md': {},
   'posts/two.md': {},
+  'posts/~draft.md': {},
   'lib/js/index.js': {},
   'bin/.git/.gitignore': {}
 }
@@ -16,11 +17,12 @@ tape('By default, all files and directories are moved up one', function (test) {
   var testFiles = lodash.cloneDeep(filesMock),
       metalsmith = lodash.cloneDeep(metalsmithMock)
 
-  test.plan(5)
+  test.plan(6)
   moveUp()(testFiles, metalsmith, function () {
     test.ok(testFiles['index.js'])
     test.ok(testFiles['one.md'])
     test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
     test.ok(testFiles['js/index.js'])
     test.ok(testFiles['.git/.gitignore'])
   })
@@ -30,15 +32,17 @@ tape('Files are moved not copied', function (test) {
   var testFiles = lodash.cloneDeep(filesMock),
       metalsmith = lodash.cloneDeep(metalsmithMock)
 
-  test.plan(8)
+  test.plan(10)
   moveUp()(testFiles, metalsmith, function () {
     test.ok(testFiles['one.md'])
     test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
     test.ok(testFiles['js/index.js'])
     test.ok(testFiles['.git/.gitignore'])
 
     test.notOk(testFiles['posts/one.md'])
     test.notOk(testFiles['posts/two.md'])
+    test.notOk(testFiles['posts/~draft.md'])
     test.notOk(testFiles['lib/js/index.js'])
     test.notOk(testFiles['bin/.git/.gitignore'])
   })
@@ -52,11 +56,12 @@ tape('custom options are supported', function (test) {
         by: 2
       }
 
-  test.plan(5)
+  test.plan(6)
   moveUp(pattern)(testFiles, metalsmith, function () {
     test.ok(testFiles['.gitignore'])
     test.ok(testFiles['posts/one.md'])
     test.ok(testFiles['posts/two.md'])
+    test.ok(testFiles['posts/~draft.md'])
     test.ok(testFiles['lib/js/index.js'])
 
     test.notOk(testFiles['bin/.git/.gitignore'])
@@ -75,14 +80,16 @@ tape('multiple options are supported', function (test) {
         by: 2
       }
 
-  test.plan(6)
+  test.plan(8)
   moveUp(patternOne, patternTwo)(testFiles, metalsmith, function () {
     test.ok(testFiles['.gitignore'])
     test.ok(testFiles['one.md'])
     test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
 
     test.notOk(testFiles['bin/.git/.gitignore'])
     test.notOk(testFiles['posts/one.md'])
     test.notOk(testFiles['posts/two.md'])
+    test.notOk(testFiles['posts/~draft.md'])
   })
 })
