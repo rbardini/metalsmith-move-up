a94fbdfa26c1ee79ecedff7d9b3c838fd6ea2c37
Dean McDonnell <mcdonnelldean@outlook.com>
2015-06-09 23:20:56 +0100

    See Description added initial support for cli improved naming added minimatch passthrough removed support for pass by args added support for more flexable params and options

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,6 +1,6 @@
 var moveUp = require('../lib/plugin'),
     tape = require('tape'),
-    lodash = require('lodash')
+    _ = require('lodash')
 
 var filesMock = {
   'index.js': {},
@@ -14,8 +14,8 @@ var filesMock = {
 var metalsmithMock = {}
 
 tape('By default, all files and directories are moved up one', function (test) {
-  var testFiles = lodash.cloneDeep(filesMock),
-      metalsmith = lodash.cloneDeep(metalsmithMock)
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock)
 
   test.plan(6)
   moveUp()(testFiles, metalsmith, function () {
@@ -28,9 +28,24 @@ tape('By default, all files and directories are moved up one', function (test) {
   })
 })
 
+tape('string input is supported', function (test) {
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock)
+
+  test.plan(6)
+  moveUp('**')(testFiles, metalsmith, function () {
+    test.ok(testFiles['index.js'])
+    test.ok(testFiles['one.md'])
+    test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
+    test.ok(testFiles['js/index.js'])
+    test.ok(testFiles['.git/.gitignore'])
+  })
+})
+
 tape('Files are moved not copied', function (test) {
-  var testFiles = lodash.cloneDeep(filesMock),
-      metalsmith = lodash.cloneDeep(metalsmithMock)
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock)
 
   test.plan(10)
   moveUp()(testFiles, metalsmith, function () {
@@ -48,16 +63,39 @@ tape('Files are moved not copied', function (test) {
   })
 })
 
-tape('custom options are supported', function (test) {
-  var testFiles = lodash.cloneDeep(filesMock),
-      metalsmith = lodash.cloneDeep(metalsmithMock),
-      pattern = {
+tape('Simple transforms are supported', function (test) {
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock),
+      transforms = [
+        'posts/*',
+        'lib/**'
+      ]
+
+  test.plan(9)
+  moveUp(transforms)(testFiles, metalsmith, function () {
+    test.ok(testFiles['one.md'])
+    test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
+    test.ok(testFiles['js/index.js'])
+    test.ok(testFiles['bin/.git/.gitignore'])
+
+    test.notOk(testFiles['posts/one.md'])
+    test.notOk(testFiles['posts/two.md'])
+    test.notOk(testFiles['posts/~draft.md'])
+    test.notOk(testFiles['lib/js/index.js'])
+  })
+})
+
+tape('custom transforms are supported', function (test) {
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock),
+      transform = {
         pattern: 'bin/.git/.gitignore',
         by: 2
       }
 
   test.plan(6)
-  moveUp(pattern)(testFiles, metalsmith, function () {
+  moveUp(transform)(testFiles, metalsmith, function () {
     test.ok(testFiles['.gitignore'])
     test.ok(testFiles['posts/one.md'])
     test.ok(testFiles['posts/two.md'])
@@ -68,20 +106,43 @@ tape('custom options are supported', function (test) {
   })
 })
 
-tape('multiple options are supported', function (test) {
-  var testFiles = lodash.cloneDeep(filesMock),
-      metalsmith = lodash.cloneDeep(metalsmithMock),
-      patternOne = {
+tape('custom minimatch options are supported', function (test) {
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock),
+      transforms = {
+        opts: {
+          dot: false
+        },
+        transforms: {
+          pattern: '**',
+          by: 2
+        }
+      }
+
+  test.plan(5)
+  moveUp(transforms)(testFiles, metalsmith, function () {
+    test.ok(testFiles['one.md'])
+    test.ok(testFiles['two.md'])
+    test.ok(testFiles['~draft.md'])
+    test.ok(testFiles['index.js'])
+    test.ok(testFiles['bin/.git/.gitignore'])
+  })
+})
+
+tape('multiple transforms are supported', function (test) {
+  var testFiles = _.cloneDeep(filesMock),
+      metalsmith = _.cloneDeep(metalsmithMock),
+      transformOne = {
         pattern: 'bin/.git/.gitignore',
         by: 2
       },
-      patternTwo = {
+      transformTwo = {
         pattern: 'posts/*',
         by: 2
       }
 
   test.plan(8)
-  moveUp(patternOne, patternTwo)(testFiles, metalsmith, function () {
+  moveUp([transformOne, transformTwo])(testFiles, metalsmith, function () {
     test.ok(testFiles['.gitignore'])
     test.ok(testFiles['one.md'])
     test.ok(testFiles['two.md'])
